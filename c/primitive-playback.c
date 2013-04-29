a239e55a95d9f0519973517e71e1b5396044f5c9
Frank Terbeck <ft@bewatermyfriend.org>
2013-04-30 01:23:43 +0200

    Keep alphabetic order
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- c/primitive-playback.c
+++ c/primitive-playback.c
@@ -10,12 +10,12 @@
 #include <libguile.h>
 #include <xmmsclient/xmmsclient.h>
 
-X2_TRIVIAL_SERVER_ACTION(x2_play, xmmsc_playback_start)
 X2_TRIVIAL_SERVER_ACTION(x2_pause, xmmsc_playback_pause)
+X2_TRIVIAL_SERVER_ACTION(x2_play, xmmsc_playback_start)
 
 void
 init_x2_primitive_playback(void)
 {
-    xg_scm_define_and_export("xmms2:primitive/play", 1, 0, 0, x2_play);
     xg_scm_define_and_export("xmms2:primitive/pause", 1, 0, 0, x2_pause);
+    xg_scm_define_and_export("xmms2:primitive/play", 1, 0, 0, x2_play);
 }
