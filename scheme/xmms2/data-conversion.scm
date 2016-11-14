fc3d34d4e185f01458e70640901834f2298b5a24
Frank Terbeck <ft@bewatermyfriend.org>
2016-11-14 01:21:03 +0100

    More complete data conversion functionality
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/xmms2/data-conversion.scm
+++ scheme/xmms2/data-conversion.scm
@@ -6,16 +6,34 @@
   #:use-module (rnrs bytevectors)
   #:export (int32-ref
             int32-set!
+            int32?
             uint32-ref
             uint32-set!
+            uint32?
             int64-ref
             int64-set!
+            int64?
             uint64-ref
-            uint64-set!))
+            uint64-set!
+            uint64?
+            *uint32-max*
+            *uint64-max*
+            *int32-max*
+            *int32-min*
+            *int64-max*
+            *int64-min*))
 
 ;; The octet order in XMMS2's protocol is big-endian. If it matters, all
 ;; procedures from this module will indeed use that particular order.
 (define *endianness* 'big)
+
+;; uint32_t
+
+(define *uint32-max* (- (ash 1 32) 1))
+
+(define (uint32? data)
+  (and (exact-integer? data)
+       (<= data *uint32-max*)))
 
 (define (uint32-ref bv offset)
 "Return an unsigned 32 bit integer from a byte-vector starting at the byte at
@@ -27,6 +45,14 @@ OFFSET."
 to DATA, which is that source unsigned integer."
   (bytevector-u32-set! bv offset data *endianness*))
 
+;; uint64_t
+
+(define *uint64-max* (- (ash 1 64) 1))
+
+(define (uint64? data)
+  (and (exact-integer? data)
+       (<= data *uint64-max*)))
+
 (define (uint64-ref bv offset)
 "Return an unsigned 64 bit integer from a byte-vector starting at the byte at
 OFFSET."
@@ -37,14 +63,15 @@ OFFSET."
 to DATA, which is that source unsigned integer."
   (bytevector-u64-set! bv offset data *endianness*))
 
-(define (int64-ref bv offset)
-"Return an 64 bit integer from a byte-vector starting at the byte at OFFSET."
-  (bytevector-s64-ref bv offset *endianness*))
+;; int32_t
 
-(define (int64-set! bv offset data)
-"Set an 64 bit integer in a byte-vector starting at the byte at OFFSET to DATA,
-which is that source integer."
-  (bytevector-s64-set! bv offset data *endianness*))
+(define *int32-max* (- (ash 1 31) 1))
+(define *int32-min* (* -1 (ash 1 31)))
+
+(define (int32? data)
+  (and (exact-integer? data)
+       (<= data *int32-max*)
+       (>= data *int32-min*)))
 
 (define (int32-ref bv offset)
   "Return an 32 bit integer from a byte-vector starting at the byte at OFFSET."
@@ -54,3 +81,22 @@ which is that source integer."
   "Set an 32 bit integer in a byte-vector starting at the byte at OFFSET to DATA,
 which is that source integer."
   (bytevector-s32-set! bv offset data *endianness*))
+
+;; int64_t
+
+(define *int64-max* (- (ash 1 63) 1))
+(define *int64-min* (* -1 (ash 1 63)))
+
+(define (int64? data)
+  (and (exact-integer? data)
+       (<= data *int64-max*)
+       (>= data *int64-min*)))
+
+(define (int64-ref bv offset)
+"Return an 64 bit integer from a byte-vector starting at the byte at OFFSET."
+  (bytevector-s64-ref bv offset *endianness*))
+
+(define (int64-set! bv offset data)
+"Set an 64 bit integer in a byte-vector starting at the byte at OFFSET to DATA,
+which is that source integer."
+  (bytevector-s64-set! bv offset data *endianness*))
