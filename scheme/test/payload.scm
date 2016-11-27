8612fa6ab6a9cb6786cc46cc05900848b7faf81c
Frank Terbeck <ft@bewatermyfriend.org>
2016-11-27 18:13:35 +0100

    tests: Add a back and forth test macro
    Takes a generator, a disector and a value, passes the value into the
    generator and turns the payload from that back into a value and checks
    if the resulting value matches the input value.
    
    The macro also takes the name of a test-tap macro that is supposed to be
    used to perform the actual test. Any excess arguments are passed to the
    test verbatim. This way you can do:
    
    Exact test for integers:
    
        (perform-payload-><-test make-int64-payload
                                 payload->int64
                                 123 pass-if-=)
    
    Inexact test for floating values:
    
        (perform-payload-><-test make-float-payload
                                 payload->float
                                 (exact->inexact (/ 2 3)
                                 pass-if-~= 1e-6)
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/test/payload.scm
+++ scheme/test/payload.scm
@@ -6,7 +6,8 @@
   #:use-module (test tap)
   #:use-module (xmms2 payload)
   #:export (pass-if-payload-equal?
-            pass-if-payload-not-equal?))
+            pass-if-payload-not-equal?
+            perform-payload-><-test))
 
 (define-syntax pass-if-payload-equal?
   (syntax-rules ()
@@ -17,3 +18,10 @@
   (syntax-rules ()
     ((_ a b)
      (pass-if-not-equal? (payload-combine a) (payload-combine b)))))
+
+(define-syntax perform-payload-><-test
+  (syntax-rules ()
+    ((_ generator disector value tap-test rest* ...)
+     (let ((v value))
+       (define-test (format #f "~a><~a: ~a" 'generator 'disector v)
+         (tap-test (disector (generator v)) v rest* ...))))))
