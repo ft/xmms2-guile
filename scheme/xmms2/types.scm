fb53cec319880827502313bfdf23c31d682d13e2
Frank Terbeck <ft@bewatermyfriend.org>
2016-12-24 14:33:18 +0100

    Add collection data-type
    This will require a couple of operations to be defined on it (it's a tree
    structure, essentially — so tree maps and all that jazz comes to mind), as well
    as a thin DSL that makes it easy to express collections without losing one's
    mind.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/xmms2/types.scm
+++ scheme/xmms2/types.scm
@@ -3,6 +3,7 @@
 ;; Terms for redistribution and use can be found in LICENCE.
 
 (define-module (xmms2 types)
+  #:use-module (srfi srfi-9)
   #:export (dictionary?
             non-complex-number?))
 
@@ -14,3 +15,11 @@
 
 (define (non-complex-number? data)
   (and (number? data) (zero? (imag-part data))))
+
+(define-record-type <collection>
+  (make-collection operator attributes idlist children)
+  collection?
+  (operator collection-operator)
+  (attributes collection-attributes)
+  (idlist collection-idlist)
+  (children collection-children))
