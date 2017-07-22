7d64207c8d0b02865f3a679e6d8ba85f9a5dbe60
Frank Terbeck <ft@bewatermyfriend.org>
2017-07-22 11:08:09 +0200

    Rewrite tools/docc in Scheme
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- tools/combine-into-markdown.scm
+++
@@ -1,7 +0,0 @@
-;; -*- scheme -*-
-
-(use-modules (documentation more)
-             (documentation combine-markdown))
-(setlocale LC_ALL "")
-(define *input* (cadr (command-line)))
-(combine-markdown *input* #:topdir (maybe-topdir "."))
--- tools/module-to-markdown.scm
+++
@@ -1,7 +0,0 @@
-;; -*- scheme -*-
-
-(use-modules (documentation module)
-             (documentation render-markdown))
-(setlocale LC_ALL "")
-(define *module* (cadr (command-line)))
-(list->markdown (module->documentation *module*))
